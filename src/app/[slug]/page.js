7fd073062e6dcb90d01e8d9f182cf082392fef6a
Sh Raj <shaswatraj3@gmail.com>
2024-03-21 06:26:02 +0000

    user profile

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -80,13 +80,13 @@ export default function ArticlesPage({ params }) {
         <section class="text-gray-600 body-font">
           <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-              <img class="object-cover object-center rounded" alt="hero" src={filteredArticles[0].user.profile_image}/>
+              {/* <img class="object-cover object-center rounded" alt="hero" src={filteredArticles && articles[0].user.profile_image}/> */}
             </div>
             <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
               <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{params.slug}
                 {/* <br class="hidden lg:inline-block"/>readymade gluten */}
               </h1>
-              <p class="mb-8 leading-relaxed">{filteredArticles[0].user.name} </p>
+              {/* <p class="mb-8 leading-relaxed">{filteredArticles && articles[0].user.name} </p> */}
               <div class="flex justify-center">
               <a href={"https://dev.to/"+params.slug}><button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">GitHub</button></a>
                 <a href={"https://dev.to/"+params.slug}><button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Dev.to</button></a>
