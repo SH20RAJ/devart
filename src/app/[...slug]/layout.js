6cae10f612bdcee4c2e769876d497f6a8569d8e8
Sh Raj <shaswatraj3@gmail.com>
2024-03-23 07:08:11 +0000

    Thats good

--- src/app/[...slug]/layout.js
+++ src/app/[...slug]/layout.js
@@ -1,4 +1,3 @@
-
 // export const metadata = {
 //   title: "op - Programming Related Articles",
 //   description: "Get Programming Related Articles on Daily Basis on DevArt a better place to spend time....",
@@ -16,33 +15,46 @@ export default async function RootLayout(pr) {
           name="google-site-verification"
           content="exvbD16MWo-o_oksJDrekaQ_zwY62YGWEA_XdlE5_XM"
         />
-<link rel="icon" href="https://github.com/fornfun.png" type="image/gif" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549"
           crossorigin="anonymous"
         ></script>
+        <title>{data.title}</title>
+        <meta name="description" content={data.description} />
+        <meta name="keywords" content={data.tag_list} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <link rel="canonical" href={"https://dev.sh20raj.com/"+data.path}/> */}
+        <meta property="og:title" content={data.title} />
+        <meta property="og:description" content={data.description} />
+        <meta property="og:image" content={data.social_image} />
+        {/* <meta property="og:url" content={"https://dev.sh20raj.com/"+data.path}/> */}
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@sh20raj" />
+        <meta name="twitter:title" content={data.title} />
+        <meta name="twitter:description" content={data.description} />
+        <meta name="twitter:image" content={data.social_image} />
+        <meta name="robots" content="index, follow" />
+        <link
+          rel="shortcut icon"
+          href="https://github.com/fornfun.png"
+          type="image/x-icon"
+        />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-CWL59Y4TR7"
         ></script>
-        <title>{data.title}</title>
-        <meta name="description" content={data.description}/>
-        <meta name="keywords" content={data.tag_list}/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        {/* <link rel="canonical" href={"https://dev.sh20raj.com/"+data.path}/> */}
-        <meta property="og:title" content={data.title}/>
-        <meta property="og:description" content={data.description}/>
-        <meta property="og:image" content={data.social_image}/>
-        {/* <meta property="og:url" content={"https://dev.sh20raj.com/"+data.path}/> */}
-        <meta property="og:type" content="website"/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content="@sh20raj"/>
-        <meta name="twitter:title" content={data.title}/>
-        <meta name="twitter:description" content={data.description}/>
-        <meta name="twitter:image" content={data.social_image}/>
-        <meta name="robots" content="index, follow"/> 
-        <link rel="shortcut icon" href="https://github.com/fornfun.png" type="image/x-icon" />
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-CWL59Y4TR7');
+        </script>
+        <script
+          type="text/javascript"
+          src="https://platform-api.sharethis.com/js/sharethis.js#property=65fe7f28fb0d80001211056b&product=sop"
+          async="async"
+        ></script>
       </head>
       <body>{children}</body>
     </html>
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,8 @@ import "./globals.css";
 
 export const metadata = {
   title: "DevArt - Programming Related Articles",
-  description: "Get Programming Related Articles on Daily Basis on DevArt a better place to spend time....",
+  description:
+    "Get Programming Related Articles on Daily Basis on DevArt a better place to spend time....",
 };
 
 export default function RootLayout({ children }) {
@@ -13,7 +14,11 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="exvbD16MWo-o_oksJDrekaQ_zwY62YGWEA_XdlE5_XM"
         />
-        <link rel="icon" href="https://github.com/fornfun.png" type="image/gif" />
+        <link
+          rel="icon"
+          href="https://github.com/fornfun.png"
+          type="image/gif"
+        />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549"
@@ -22,6 +27,16 @@ export default function RootLayout({ children }) {
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-CWL59Y4TR7"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-CWL59Y4TR7');
+        </script>
+        <script
+          type="text/javascript"
+          src="https://platform-api.sharethis.com/js/sharethis.js#property=65fe7f28fb0d80001211056b&product=sop"
+          async="async"
         ></script>
       </head>
       <body>{children}</body>
