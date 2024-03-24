341564e511f6c066470d01b83c618448d701f5f3
Sh Raj <shaswatraj3@gmail.com>
2024-03-24 06:01:10 +0000

    all set

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -7,7 +7,7 @@ export default () => {
     <b>
       @Blog<span style={{ color: "tomato" }}>X</span>{" "}
     </b>{" "}
-    | Created with ❤️ by <a href="http://github.com/sh20raj">@sh20raj</a> | <a href="https://codexdindia.blogspot.com/" target="_">CXDIIN</a>
+    | Created with ❤️ by <a href="http://github.com/sh20raj">@sh20raj</a> | <a href="https://codexdindia.blogspot.com/" target="_">CXDIIN</a> | <a href="/devart/guidelines-and-terms-of-service-tos-4khk">TOS</a>
   </footer>
     )
 }
