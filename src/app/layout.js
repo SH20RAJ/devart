31a74727d4c5ecadc0bbea470ea75268ef0ce799
Sh Raj <shaswatraj3@gmail.com>
2024-03-26 03:00:54 +0000

    posthog analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
 import Script from "next/script";
 
-
 export default function RootLayout({ children }) {
   let ga_id = "G-CWL59Y4TR7";
   return (
@@ -52,6 +51,12 @@ export default function RootLayout({ children }) {
           src="https://platform-api.sharethis.com/js/sharethis.js#property=65fe7f28fb0d80001211056b&product=sop"
           async="async"
         ></script>
+                <Script
+          dangerouslySetInnerHTML={{
+            __html: `!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+            posthog.init('phc_gwuiWCosj2Qgm2i5I4p1gS6RiBogbWNc0Jd3wEl5tIq',{api_host:'https://app.posthog.com'})`,
+          }}
+        ></Script>
       </head>
       <body>{children}</body>
     </html>
