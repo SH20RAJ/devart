74a468c0bf47ea3f58b407df7269c1f9916b4cae
Sh Raj <shaswatraj3@gmail.com>
2024-03-28 18:15:15 +0530

    Analytics Added
    ....

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -10,6 +10,8 @@ export default () => {
       @Blog<span style={{ color: "tomato" }}>X</span>{" "}
     </b>{" "}
     | Created with ❤️ by <a href="http://github.com/sh20raj">@sh20raj</a> | <a href="https://codexdindia.blogspot.com/" target="_">CXDIIN</a> | <a href="/devart/guidelines-and-terms-of-service-tos-4khk">TOS</a>
+<a href="https://visitorbadge.io/status?path=DevArt" class="hidden"><img src="https://api.visitorbadge.io/api/visitors?path=DevArt&countColor=%23263759" /></a>
+<img src="https://bit.ly/DevArtTracker" class="hidden" />
   </footer>
     )
 }
