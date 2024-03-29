ccf83a79db4fd28f034674962534f23739775830
sh20raj <shaswatraj3@gmail.com>
2024-03-29 02:51:51 +0000

    80

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ export default function ArticlesPage() {
       setIsLoading(true);
       try {
         const response = await fetch(
-          `https://dev.to/api/articles/latest/?per_page=22&page=${page}`
+          `https://dev.to/api/articles/latest/?per_page=80&page=${page}`
         );
         const data = await response.json();
         setArticles((prevArticles) => [...prevArticles, ...data]);
