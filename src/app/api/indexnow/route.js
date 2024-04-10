ffa937463e19e4a1a76f8ae75f70cb84d0629c68
Sh Raj <shaswatraj3@gmail.com>
2024-04-10 11:47:25 +0000

    indexnow api aded

---
+++ src/app/api/indexnow/route.js
@@ -0,0 +1,62 @@
+// Replace with your actual API key and URL
+const apiKey = '11bf36c257104618b69465f71c342d6c';
+let urlToSubmit = 'https://sh20raj.com/anishamalde/a-love-letter-to-gen-z-from-gen-y-understanding-reacts-evolution-4abm';
+
+
+
+
+export async function GET(req,res){
+    const url = new URL(req.url)
+
+    let host = url.host || 'sh20raj.com';
+    let keyLocation = url.origin + "/" + apiKey + ".txt" || 'https://sh20raj.com/11bf36c257104618b69465f71c342d6c.txt';
+    urlToSubmit = url.searchParams.has("url") ? url.searchParams.get("url") : urlToSubmit;
+
+     // Construct the API request payload
+    const payload = {
+        host: host,
+        key: apiKey,
+        keyLocation: keyLocation,
+        urlList: [urlToSubmit],
+    };
+
+let indexit = async () => {
+let msg = "__"
+fetch('https://api.indexnow.org/IndexNow', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+})
+    .then((response) => {
+        if (response.ok) {
+            console.log(`URL ${urlToSubmit} submitted successfully to IndexNow.`);
+             msg = `URL ${urlToSubmit} submitted successfully to IndexNow.`;
+
+            // return response.json()
+        } else {
+            console.error(`Error submitting URL ${urlToSubmit}. Status code: ${response.status}`);
+            // return response.json(); // returning the response JSON for further examination
+        }
+    })
+    .then((data) => {
+        if (data) {
+            console.error('Error response from server:', data);
+        }
+    })
+    .catch((error) => {
+        console.error('An error occurred:', error.message);
+    });
+
+
+}
+// Make an HTTP POST request to the IndexNow API
+
+
+        
+    return Response.json({msg:"submitted",indexdata: await indexit(),host : host, apiKey: apiKey,keyLocation: keyLocation, url: urlToSubmit})
+
+
+}
+
