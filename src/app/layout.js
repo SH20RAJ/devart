54040ed1884516cb16d5500284fb73b5eedddc5f
Sh Raj <shaswatraj3@gmail.com>
2024-04-17 17:53:22 +0530

    Added YandexMeta

--- src/app/layout.js
+++ src/app/layout.js
@@ -61,6 +61,35 @@ export default function RootLayout({ children }) {
       </head>
       <body>{children}</body>
       <Analytics/>
+          <yadexmat/>
+          
     </html>
   );
 }
+
+
+export const yadexmeta = async () => {
+  let i = `
+  <script>
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+  ym(97056210, "init", {
+       clickmap:true,
+       trackLinks:true,
+       accurateTrackBounce:true,
+       webvisor:true,
+       ecommerce:"dataLayer"
+  });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/97056210" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+  `
+return (
+  <>
+  <div  dangerouslySetInnerHTML={{ __html : i}}></div>
+  </>
+)}
